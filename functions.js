fd20f7c881a0bebf0806c84296ae882d55481ac3
Your NameAftan <aftan.merrida@gmail.com>
2018-08-25 11:19:26 -0500

    first commit functions

---
+++ functions.js
@@ -0,0 +1,33 @@
+//input=animal example fish
+//output= animal product   example fish stix
+//Below is the syntax
+
+
+function nuggetizer(animal){ 
+
+return `${animal} + stix`;
+}
+
+
+console.log ('fish:', nuggetizer ('fish'));
+console.log ('cat:', nuggetizer ('cat'));   
+console.log ('bear:', nuggetizer ('bear'));
+
+const nuggtizer =(animal)=> {
+
+};
+
+
+const nuggetizer2= (animal) => {
+    return`${animal} jerky`
+};
+console.log ('deer:',nuggetizer2('deer'));
+
+const bearSticks =nuggetizer2('bear');
+
+
+const nomnom = (person,food) => {
+    return `${person} devoured ${food}`;
+
+}
+console.log (nomnom(`aftan`, bearSticks));
