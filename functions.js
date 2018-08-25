fa7e60068d5f87ca7e98993f367349efa4854688
Your NameAftan <aftan.merrida@gmail.com>
2018-08-25 11:49:14 -0500

    dom functions

--- functions.js
+++ functions.js
@@ -31,3 +31,24 @@ const nomnom = (person,food) => {
 
 }
 console.log (nomnom(`aftan`, bearSticks));
+
+
+const printToDom =(stringToPrint,divId)=> {
+const selectdDiv =document.getElementById (divId)
+selectdDiv.innerHTML += stringToPrint;
+};
+
+//we will have this in every project we do for the next 2 months//
+
+
+const numberadder = (x) => {
+    const finalNumber = x + 3;
+    printToDom(`<h2>${finalNumber}</h2>`,'allTheNumbers')
+}
+
+numberadder(22); //25
+numberadder(2);// 5
+numberadder(4);
+
+
+printToDom(`I am ready for lunch`, `feelings`)
